b525d51459531216444f19c4c7231c6c7e8a34b2
azzelden12 <163587106+azzelden12@users.noreply.github.com>
2024-04-14 17:07:21 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('.menu i');
+const dropDown = document.querySelector('.dropdown');
+menu.onclick = function () {
+    dropDown.classList.toggle("open")
+};
+
+const navEl = document.querySelector('.containe');
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY >= 56) {
+        navEl.classList.add('scroll');
+    }else if (window.scrollY < 56) {
+        navEl.classList.remove('scroll')
+    }
+});
